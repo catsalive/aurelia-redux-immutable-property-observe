04350a2e20e2dc0f2cd38c58d32761f0a4c6a54d
Grif Thomas <gthomas.1978@gmail.com>
2017-08-14 11:26:28 -0700

    fixed system

--- dist/system/index.js
+++ dist/system/index.js
@@ -26,6 +26,28 @@ System.register(['lodash'], function (_export, _context) {
         store = aurelia.container.get(storeInstance);
       });
 
+      var getKeys = function getKeys(obj) {
+        var keys = [];
+        for (var prop in obj) {
+          keys.push(prop);
+        }
+        return keys;
+      }
+
+      var combineArrays = function combineArrays(array1, array2) {
+        var combinedArray = array1 ? array1.slice(0) : [];
+        array2.forEach(function(val) {
+          var bFound = false;
+          combinedArray.forEach(function(val1) {
+            bFound = true;
+          });
+          if (!bFound) {
+            combinedArray.push(val);
+          }
+        });
+        return combinedArray;
+      }
+      
       connect = function connect(viewModel, stateMapper, mapByKeyLevel) {
         if (typeof mapByKeyLevel !== 'number' && typeof mapByKeyLevel !== 'undefined') { throw new Error('mayByKeyLevel must be a number'); }
         stateMapper = stateMapper || function (state) {
@@ -39,9 +61,12 @@ System.register(['lodash'], function (_export, _context) {
           if (!mapByKeyLevel) {
             viewModel.state = mappedState;
           } else {
-            Object.keys(mappedState).forEach(function(key, index) {
+            var mappedKeys = getKeys(mappedState);
+            mappedKeys.forEach(function(key, index) {
               if (mapByKeyLevel > 1 && typeof mappedState[key] === 'object' && typeof viewModel[key] === 'object' && mappedState[key] !== null && viewModel[key] !== null) {
-                Object.keys(viewModel[key]).concat(Object.keys(mappedState[key])).filter((value, ix, array) => array.indexOf(value) === ix).forEach((k) => {
+                var vmPropKeys = getKeys(viewModel[key]);
+                var msPropKeys = getKeys(mappedState[key]);
+                combineArrays(vmPropKeys, msPropKeys).forEach(function(k) {
                   viewModel[key][k] = mappedState[key][k];
                 });
               } else {
